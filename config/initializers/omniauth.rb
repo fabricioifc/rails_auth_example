983a3c51d4c6c2ef71e065c6729f0cd5ea09bd5d
Fabricio <fabricio.bizotto@ifc.edu.br>
2026-04-24 08:46:17 -0300

    asd

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: "user:email"
-  provider :google_oauth2, 
-    ENV['GOOGLE_CLIENT_ID'], 
-    ENV['GOOGLE_CLIENT_SECRET'], 
-    scope: "userinfo.email, userinfo.profile"
-  provider :twitter2, ENV['X_CLIENT_ID'], ENV['X_CLIENT_SECRET'],
-   scope: "tweet.read users.read users.email",
+  provider :github, 
+    ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], 
+    scope: "user:email"
+  provider :twitter2, 
+    ENV['X_CLIENT_ID'], ENV['X_CLIENT_SECRET'],
+    scope: "tweet.read users.read users.email",
          callback_path: "/auth/twitter2/callback"
 end
 
